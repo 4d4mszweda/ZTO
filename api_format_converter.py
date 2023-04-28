6af275e8e17b62b381aad82ea68647962b4f93c6
Adam <maxsuspl@gmail.com>
2023-04-28 17:01:27 +0200

    seperate in function

--- api_format_converter.py
+++ api_format_converter.py
@@ -2,22 +2,25 @@ def main():
     with open("TECDOC.txt", "r", encoding="utf8") as tecdoc:
         with open("result.txt", "w") as result:
             read = tecdoc.readlines()
+            data = getApiData(read)
+            
+
+                    
+def getApiData(txt):
+    final = []
+    temp = []
+    for line in txt:
+        if("Typ pojazdu" in line):
+            temp.append(line[12:])
+        if("Rok produkcji" in line and "pojazdu" not in line):
+            temp.append(line[14:])
+        if("Pojemność skokowa" in line):
+            temp.append(line[18:])
+        if("Kody silników" in line):
+            temp.append(line[14:])
+            final.append(temp)
             temp = []
-            final = []
-            for count, line in enumerate(read):
-                if("Typ pojazdu" in line):
-                    temp.append(line[12:])
-                if("Rok produkcji" in line and "pojazdu" not in line):
-                    temp.append(line[14:])
-                if("Pojemność skokowa" in line):
-                    temp.append(line[18:])
-                if("Kody silników" in line):
-                    temp.append(line[14:])
-                    final.append(temp)
-                    temp = []
-            print(final)
-                    
-                    
+    return final
 
 if __name__ == "__main__":
     main()
