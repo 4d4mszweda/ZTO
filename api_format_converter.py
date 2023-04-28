6c3fd7699c7d3b8354d516bc61a4641cb83bda3c
Adam <maxsuspl@gmail.com>
2023-04-28 18:55:06 +0200

    powoli do przodu

--- api_format_converter.py
+++ api_format_converter.py
@@ -5,22 +5,28 @@ def main():
         with open("result.txt", "w") as result:
             read = tecdoc.readlines()
             data = getApiData(read)
+            # printList(data)
             data = formatApi(data)
+            printList(data)
+            
             
 def formatApi(list):
     for element in list:
-        element[0] = re.sub(" \(.*?\)", "", element[0])
+        element[1] = re.sub(" \(.*?\)", "", element[1])
         element[0] = re.sub("\\n", "", element[0])
         element[1] = re.sub("\\n", "", element[1])
         element[2] = re.sub("\\n", "", element[2])
         element[3] = re.sub("\\n", "", element[3])
-    print(list)
-    return
+        element[4] = re.sub("\\n", "", element[4])
+    return list
                     
 def getApiData(txt):
     final = []
     temp = []
-    for line in txt:
+    for count, line in enumerate(txt):
+        if(" Numer części OE" in line):
+            name = txt[count + 1]
+            temp.append(name)
         if("Typ pojazdu" in line):
             temp.append(line[12:])
         if("Rok produkcji" in line and "pojazdu" not in line):
@@ -31,7 +37,12 @@ def getApiData(txt):
             temp.append(line[14:])
             final.append(temp)
             temp = []
+            temp.append(name)
     return final
+
+def printList(list):
+    for element in list:
+        print(element)
 
 if __name__ == "__main__":
     main()
