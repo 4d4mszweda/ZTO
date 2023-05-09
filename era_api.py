d7e8961b8db51fc86c16e3d2e38a9098694facff
Adam <maxsuspl@gmail.com>
2023-05-09 20:28:35 +0200

    delete

--- era_api.py
+++ era_api.py
@@ -1,5 +1,12 @@
+import requests
+import re
+
 def main():
-    return
+    # era = requests.get("https://ecom.eraspares.com/ec/newprint.asp?IdReferenza=1865")
+    era = requests.get("https://en.voltag.ru/catalog/group/voltag_alb2825_generator/?q=F000BL0841")
+    era = era.text
+    era = re.sub("<.*?>", "", era)
+    print(era)
 
 if __name__ == "__main__":
     main()
